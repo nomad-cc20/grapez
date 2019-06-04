493721b0ecc49b4d1b295500b5ad9f9c9f19349c
nomad <st46619@student.upce.cz>
2019-06-04 21:10:43 +0200

    funkncni konvolucni

--- sem/main_convolution.py
+++ sem/main_convolution.py
@@ -1,4 +1,8 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras_preprocessing import image
+import numpy as np
+import os
+from tqdm import tqdm
 
 from sem.net_convolution import CNN
 
@@ -6,17 +10,17 @@ from sem.net_convolution import CNN
 # img parameters
 train_path = 'img/train'
 test_path = 'img/test'
-height = 28
-width = 28
+height = 40
+width = 40
 depth = 3
 bits = 8
 
 classes = 10 # count of result classes
 
 batch_size = 30 # learning batch
-epochs = 1000
+epochs = 100
 
-net_c = CNN(32, (3, 3), (height, width, depth), (1, 1), 2, 3)
+net_c = CNN(32, (3, 3), (height, width, depth), (1, 1), 1, 3)
 
 # batch operation
 train_datagen = ImageDataGenerator(
@@ -53,3 +57,21 @@ test_generator = test_datagen.flow_from_directory(
 # resulting number of images = steps * batch_size
 out = net_c.evaluate_generator(test_generator, 1000)
 print(out)
+
+
+path = 'img/'
+testFolder = 'test/'
+folders = {'pos', 'neg', 'env'}
+
+i = 0
+results = np.zeros((3, 200))
+for folderName in os.listdir(path + testFolder):
+    j = 0
+    for fileName in tqdm(os.listdir(path + testFolder + '/' + folderName)):
+        results[i, j] = np.argmax(net_c.predict(path + testFolder + '/' + folderName + '/' + fileName, bits))
+        #print(fileName)
+        #print(net_c.predict(path + testFolder + '/' + folderName + '/' + fileName, bits))
+        j = j + 1
+    i = i + 1
+
+print(results)
